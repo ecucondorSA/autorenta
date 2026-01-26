acf33e8a754e2bd9efe2c04733a9e8f989bcbbf3
ecucondorSA <ecucondor@gmail.com>
2026-01-26 07:50:12 -0300

    fix(ci): auto-repair by Tier 6 Brain 🧠

--- apps/web/src/app/shared/components/car-carousel/car-carousel.component.ts
+++ apps/web/src/app/shared/components/car-carousel/car-carousel.component.ts
@@ -1,31 +1,45 @@
-import { Component, Input } from '@angular/core';
-import { CarMiniCardComponent } from '../car-mini-card/car-mini-card.component';
-import { CommonModule } from '@angular/common';
-import { SwiperContainer } from 'swiper/element';
+import { Component, Input, AfterViewInit, ViewChild, ElementRef } from '@angular/core';
 import { register } from 'swiper/element/bundle';
 register();
 
+import { CarMiniCardComponent } from '../car-mini-card/car-mini-card.component';
+import { Car } from '@core/models/car.model';
+
 @Component({
   selector: 'app-car-carousel',
-  standalone: true,
-  imports: [CarMiniCardComponent, CommonModule],
-  template: `
-    <swiper-container
-      class="mySwiper"
-      pagination="true"
-      navigation="true"
-      space-between="30"
-      slides-per-view="1"
-    >
-      <swiper-slide *ngFor="let car of cars">
-        <app-car-mini-card [car]="car"></app-car-mini-card>
-      </swiper-slide>
-    </swiper-container>
-  `,
-  styleUrls: ['./car-carousel.component.scss'],
+  templateUrl: './car-carousel.component.html',
+  styleUrls: ['./car-carousel.component.scss']
 })
-export class CarCarouselComponent {
-  @Input() cars: any[];
+export class CarCarouselComponent implements AfterViewInit {
+  @Input() cars: Car[] = [];
+  @ViewChild('swiperEl') swiperElRef: ElementRef | undefined;
 
-  constructor() {}
+  swiperParams = {
+    slidesPerView: 'auto',
+    spaceBetween: 10,
+    breakpoints: {
+      640: {
+        slidesPerView: 'auto',
+        spaceBetween: 20,
+      },
+      768: {
+        slidesPerView: 'auto',
+        spaceBetween: 30,
+      },
+      1024: {
+        slidesPerView: 'auto',
+        spaceBetween: 30,
+      }
+    }
+  };
+
+  constructor() { }
+
+  ngAfterViewInit(): void {
+    if (this.swiperElRef) {
+      Object.assign(this.swiperElRef.nativeElement, this.swiperParams);
+
+      this.swiperElRef.nativeElement.initialize();
+    }
+  }
 }
